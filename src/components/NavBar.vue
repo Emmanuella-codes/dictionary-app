f8891d6d852d8a95eb47cea3c81235ad3ce2f6ce
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-06-22 13:14:26 +0100

    navbar components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,85 +1,28 @@
 <script setup>
-import { useDark, useToggle } from '@vueuse/core'
+/* import { useDark, useToggle } from '@vueuse/core' */
+import FontSelector from './navCmp/FontSelector.vue'
+import ToggleBtn from './navCmp/ToggleBtn.vue'
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark);
-console.log(isDark)
+// const isDark = useDark()
+// const toggleDark = useToggle(isDark)
+// console.log(isDark)
 </script>
 
 <template>
-  <nav>
-    <!--logo-->
-    <div>
-      <img src="../assets/da-logo.png" alt="logo" />
-    </div>
-    <!--div container for fonts and color toggle-->
-    <div>
-      <div></div>
-      <div>
-        <label @click="toggleDark()" class="switch">
-          <input type="checkbox" />
-          <span class="slider"></span>
-        </label>
+  <nav id="navbar">
+    <div
+      class="flex flex-row lg:mx-auto max-w-[90%] lg:w-[80%] items-center justify-between"
+    >
+      <!--logo-->
+      <div class="flex justify-start">
+        <img src="../assets/da-logo.png" class="w-full lg:w-2/4" alt="logo" />
+      </div>
+      <!--div container for fonts and color toggle-->
+      <div class="flex gap-3 lg:gap-6 items-center">
+        <FontSelector />
+
+        <ToggleBtn />
       </div>
     </div>
   </nav>
 </template>
-
-<style scoped>
-.switch {
-  --secondary-container: #3a4b39;
-  --primary: #84da89;
-  font-size: 17px;
-  position: relative;
-  display: inline-block;
-  width: 3.7em;
-  height: 1.8em;
-}
-
-.switch input {
-  display: none;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #313033;
-  transition: 0.2s;
-  border-radius: 30px;
-}
-
-.slider:before {
-  position: absolute;
-  content: '';
-  height: 1.4em;
-  width: 1.4em;
-  border-radius: 20px;
-  left: 0.2em;
-  bottom: 0.2em;
-  background-color: #aeaaae;
-  transition: 0.4s;
-}
-
-input:checked + .slider::before {
-  background-color: var(--primary);
-}
-
-input:checked + .slider {
-  background-color: var(--secondary-container);
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px var(--secondary-container);
-}
-
-input:checked + .slider:before {
-  transform: translateX(1.9em);
-}
-</style>
---
+++ src/components/navCmp/FontSelector.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      selectedFont: 'serif'
+    }
+  },
+  methods: {
+    changeFontFamily() {
+      document.documentElement.style.fontFamily = this.selectedFont
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <label for="font-family"></label>
+    <select
+      id="font-family"
+      v-model="selectedFont"
+      @change="changeFontFamily"
+      class="border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 py-3 px-2"
+    >
+      <option value="serif">Serif</option>
+      <option value="sans-serif">Sans-serif</option>
+      <option value="cursive">Cursive</option>
+      <option value="fantasy">Fantasy</option>
+      <option value="monospace">Monospace</option>
+    </select>
+  </div>
+</template>
